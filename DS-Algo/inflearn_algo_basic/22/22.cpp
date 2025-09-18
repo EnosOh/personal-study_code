7d49cfbf5c0d283effea79d5e457ebf13962a487
EnosOh <otj111000@gmail.com>
2025-09-18 23:59:04 +0900

    feat(22) : ing

---
+++ DS-Algo/inflearn_algo_basic/22/22.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int N, K;
+    cin >> N >> K;
+
+    vector<int> temperature_vector(N + 1);
+    for (int i = 1; i <= N; i++)
+        cin >> temperature_vector[i];
+
+    int max = 0;
+    for(int i=1;i<=K-1;i++)
+        max+=temperature_vector[i];
+    
+    int temp_total = max;
+    for (int i = K+1; i <= N; i++)
+    {
+        temp_total = temp_total + temperature_vector[i] - temperature_vector[(i - K)];
+        if(max < temp_total)
+            max = temp_total;
+    }
+
+    cout << max;
+    return 0;
+}
